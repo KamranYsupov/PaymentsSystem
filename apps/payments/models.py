1eec5e4149d850df1653042bc92c930cad94f51b
KamranYsupov <kamrankek48@gmail.com>
2025-05-29 14:09:11 +0400

    модели

--- apps/payments/models.py
+++ apps/payments/models.py
@@ -1,3 +1,43 @@
-from django.db import models
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.translation import gettext_lazy as _
+
+
+
+class Organization(models.Model):
+    """Модель организации"""
+    inn = models.CharField(
+        _('ИНН'),
+        max_length=20,
+        unique=True,
+        db_index=True
+    )
+    balance = models.PositiveBigIntegerField(
+        _('Баланс'),
+        default=0
+    )
+
+
+class Payment(models.Model):
+    """Модель платежа"""
+    amount = models.PositiveBigIntegerField(_('Сумма'))
+    payer_inn = models.CharField(
+        _('ИНН плательщика'),
+        max_length=20,
+    )
+    document_number = models.CharField(
+        _('Номер документа'),
+        max_length=20
+    )
+    document_date = models.DateField(_('Дата документа'))
+
+
+class Operation(models.Model):
+    """Модель операции"""
+    id = models.UUIDField(
+        primary_key=True,
+        db_index=True,
+        default=uuid.uuid4,
+        editable=False,
+    )
